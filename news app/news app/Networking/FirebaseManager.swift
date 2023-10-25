58a809e65faa669d05f74ed5f7ec0f1167224e13
dtrognn <dtrognn@gmail.com>
2023-10-25 11:32:29 +0700

    feature config

---
+++ news app/news app/Networking/FirebaseManager.swift
@@ -0,0 +1,33 @@
+//
+//  FirebaseManager.swift
+//  news app
+//
+//  Created by dtrognn on 25/10/2023.
+//
+
+import FirebaseAuth
+import Foundation
+
+class FirebaseManager {
+    func register<T>(with email: String, password: String, complete: @escaping (_ result: Result<T, Error>) -> Void) {
+        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+            if let error = error {
+                complete(.failure(error))
+            } else {
+                guard let result = result as? T else { return }
+                complete(.success(result))
+            }
+        }
+    }
+
+    func login<T>(with email: String, password: String, complete: @escaping (_ result: Result<T, Error>) -> Void) {
+        Auth.auth().signIn(withEmail: email, password: password) { result, error in
+            if let error = error {
+                complete(.failure(error))
+            } else {
+                guard let result = result as? T else { return }
+                complete(.success(result))
+            }
+        }
+    }
+}
