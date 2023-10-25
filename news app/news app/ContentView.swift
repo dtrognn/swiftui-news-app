f078265cefc2586dbcdf2f24c1becc333ad2cccf
dtrognn <dtrognn@gmail.com>
2023-10-25 10:23:38 +0700

    init project

---
+++ news app/news app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  news app
+//
+//  Created by dtrognn on 25/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ news app/news app/news_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  news_appApp.swift
+//  news app
+//
+//  Created by dtrognn on 25/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct news_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
